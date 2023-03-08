287c99adbcab55ef8cbc3272070e3d7d615ccda5
Vitor MeirelesP <pintorgamer12@gmail.com>
2023-03-08 11:46:20 -0300

    aula 2,3 4(ainda n finalizada)

--- AULA3.sql
+++ AULA3.sql
@@ -1,3 +1,8 @@
+/*
 DISTINCT: é usado para omitir os dados duplicados de uma tabela 
 SELECT DISTINCT coluna1,coluna2
 FROM tabela
+### Desafio 2####
+Quantos nomes sobrenomes únicos temos em nossa tabela person.Person ?
+1206
+*/
